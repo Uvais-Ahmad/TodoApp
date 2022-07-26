a22620a30df8d5c7f882015b7dc67c4a21e481e4
Uvais Ahmad <uvss21@gmail.com>
2022-07-26 18:36:17 +0530

    Add router and controller

---
+++ controller/home.js
@@ -0,0 +1,3 @@
+module.exports.homeAction = function(req , res ){
+    res.end('<h1>This is response gives from the servern TodOApp</h1>');
+}
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+app.get('/',require('./routers'));
+
 app.listen(port , function(err){
     if(err){ console.log(`Error Occurs while running server on port ${port}`);}
     console.log('Todo App  server running...');
