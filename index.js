8d4c701b77578890d34b8ca55bacf9aabca5f375
Uvais Ahmad <uvss21@gmail.com>
2022-07-26 15:39:59 +0530

    SetUp DirStruc and create index.js File root file

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+app.listen(port , function(err){
+    if(err){ console.log(`Error Occurs while running server on port ${port}`);}
+    console.log('Todo App  server running...');
+});
